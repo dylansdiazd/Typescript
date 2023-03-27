bd24cfa478c72b0f96b1ae2fdf2eda5569f18b44
diaz <dylansantosdiaz@hotmail.com>
2023-03-27 01:52:31 -0300

    Desafios de introdução Typescript

---
+++ Desafios-TypeScript/desafio1.ts
@@ -0,0 +1,8 @@
+let employee = {
+    code: 22,
+    name: 'Dylan'
+};
+employee.code = 10;
+employee.name = "John";
+
+console.log(employee)
---
+++ Desafios-TypeScript/desafio3.ts
@@ -0,0 +1,43 @@
+let botaoAtualizar = document.getElementById('atualizar-saldo')as HTMLButtonElement;
+let botaoLimpar = document.getElementById('limpar-saldo') as HTMLButtonElement;
+let soma = document.getElementById('soma') as HTMLInputElement;
+let campoSaldo = document.getElementById('campo-saldo') as HTMLSpanElement ;
+
+campoSaldo.innerHTML = '0'
+
+function somarAoSaldo(soma) {
+    campoSaldo.innerHTML += soma;
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(soma.value);
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
+
+/* 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h4>Valor a ser adicionado: <input id="soma"> </h4>
+    <button id="atualizar-saldo">Atualizar saldo</button>
+    <button id="limpar-saldo">Limpar seu saldo</button>
+    <h1>"Seu saldo é: " <span id="campo-saldo"></span></h1>
+
+    <script src="desafio3.ts"></script>
+</body>
+</html>
+*/
